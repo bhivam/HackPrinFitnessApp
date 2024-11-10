2a573aeb51a77f0ccec72ccd642dadf58e67bcb7
Rahil <rahildhodapkar@gmail.com>
2024-11-09 21:15:13 -0500

    cool home layout

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Image, View, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { WebView } from 'react-native-webview';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { images } from '../../constants';
+
+const treePose = images.treePose;
 
 const Home = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>My Spline Scene</Text>
-      <View style={styles.webviewContainer}>
-        <WebView 
-          source={{ uri: 'https://prod.spline.design/G8QAMi4SlaROOVDN/scene.splinecode' }} 
-          style={styles.webview}
-        />
-      </View>
+    <SafeAreaView className='relative h-full' style={styles.container} edges={[]}>
+      <LinearGradient
+        colors={[
+          'hsl(240, 100%, 20%)',
+          'hsl(274, 100%, 21%)',
+          'hsl(293, 100%, 21%)',
+          'hsl(309, 100%, 24%)',
+          'hsl(320, 100%, 29%)',
+          'hsl(327, 100%, 34%)',
+          'hsl(333, 100%, 38%)',
+          'hsl(338, 100%, 41%)',
+          'hsl(343, 96%, 45%)',
+          'hsl(358, 84%, 56%)',
+          'hsl(10, 92%, 57%)',
+          'hsl(20, 97%, 55%)',
+          'hsl(30, 100%, 50%)',
+        ]}
+        style={styles.gradientBackground}
+      >
+        <View className='h-full absolute bottom-0 left-0 right-0 align-middle w-full'>
+          <Text className="text-3xl mt-16 font-psemibold text-white text-center">Choose an Exercise</Text>
+          <Image source={images.orb} className='max-h-[30vh] w-full m-0 p-0 absolute bottom-0' />
+        </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
@@ -20,21 +40,11 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
-  header: {
-    fontFamily: 'System', 
-    textAlign: 'center',
-    fontSize: 24, 
-    marginVertical: 20,
-  },
-  webviewContainer: {
-    flex: 1,
-    marginHorizontal: 10, 
-  },
-  webview: {
+  gradientBackground: {
     flex: 1,
   },
 });
 
 export default Home;
+
--- constants/images.js
+++ constants/images.js
@@ -1,10 +1,25 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
-import cards from "../assets/images/cards.png";
-import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/logo-small.png";
-import empty from "../assets/images/empty.png";
-import mascot from "../assets/images/mascot.png";
+import profile from '../assets/images/profile.png';
+import thumbnail from '../assets/images/thumbnail.png';
+import cards from '../assets/images/cards.png';
+import path from '../assets/images/path.png';
+import logo from '../assets/images/logo.png';
+import logoSmall from '../assets/images/logo-small.png';
+import empty from '../assets/images/empty.png';
+import mascot from '../assets/images/mascot.png';
+import orb from '../assets/images/orb.png';
+import squatPose from '../assets/images/squat-pose.png';
+import treePose from '../assets/images/tree-pose.png';
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty, mascot };
+export default {
+  profile,
+  thumbnail,
+  cards,
+  path,
+  logo,
+  logoSmall,
+  empty,
+  mascot,
+  orb,
+  squatPose,
+  treePose,
+};
